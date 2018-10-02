c7f1b3c9296635cbb34a85eaef9943517cc77110
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2018-10-02 11:58:23 +0900

    :sparkles: link endpoint lambda with main lambda

--- src/google-search/handler.js
+++ src/google-search/handler.js
@@ -1,15 +1,48 @@
 'use strict';
 
-module.exports.index = async (event, context) => {
+const AWS = require('aws-sdk');
+
+module.exports.index = async (event, context, callback) => {
     console.log(context);
-    return {
-        statusCode: 202,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
+    const token = JSON.parse(event.body).TOKEN;
+    if (token != process.env.TOKEN) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'bad request',
+                input: event.body
+            }),
+        };
+    }
+
+    const lambda = new AWS.Lambda();
+    const params = {
+        FunctionName: 'google-serach-dev-google-serach',
+        ClientContext: 'google-serach-endpoint',
+        InvocationType: 'Event',
+        Payload: JSON.stringify({
+            args: {"test": "hoge"}
         }),
     };
 
+    await lambda
+        .invoke(params)
+        .promise()
+        .then(function() {
+            const response = {
+                statusCode: 202,
+                body: JSON.stringify({
+                    message: 'Go Serverless v1.0! Your function executed successfully!',
+                    input: event.body,
+                }),
+            };
+            console.log('lambda will be ended with success');
+            return callback(null, response);
+        })
+        .catch(function(error) {
+            console.log('lambda will be ended with failure');
+            return callback(error);
+        });
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
